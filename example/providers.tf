6e07ed4d78e177ab91463e7112019c4a99c87431
franklinpashok <franklinpashok@gmail.com>
2023-07-26 11:47:01 +0800

    update providers

--- example/providers.tf
+++ example/providers.tf
@@ -1,10 +1,5 @@
 provider "aws" {
   region = "ap-southeast-1"
-
-  skip_metadata_api_check = true
-
-  allowed_account_ids = ["1234567890"]
-
   default_tags {
     tags = {
     }
@@ -14,10 +9,6 @@ provider "aws" {
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
-
-  allowed_account_ids = [
-    "1234567890",
-  ]
 
   default_tags {
     tags = {
