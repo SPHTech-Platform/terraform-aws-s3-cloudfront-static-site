fc6de4b19cf629cf85b9b43dfd523635c8976865
franklinpashok <franklinpashok@gmail.com>
2024-04-29 19:14:04 +0800

    Add condition to associate the default function

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  default_function_association = length(var.default_cache_behavior) == 0 ? {
+    function_association = {
+      viewer-request = {
+        function_arn = aws_cloudfront_function.viewer_request[0].arn
+      }
+    }
+  } : {}
+}
